7ce64ebae3f4ff47efa064bfbccd9275f78d339b
i.seliverstov <noonon.me@gmail.com>
2016-07-20 17:16:22 +0300

    style add

--- frontend/components/helloWord/index.jsx
+++ frontend/components/helloWord/index.jsx
@@ -4,9 +4,16 @@
 
 export default class helloWord {
     constructor(React) {
+
+        require('./style/index.styl');
+
         return React.createClass({
             render() {
-                return <h1>Hello</h1>
+                return <div className="hello-world">
+                    <div className = "hello-world__item">
+                        <div className = "hello-world__item-title" >Hello Word</div>
+                    </div>
+                </div>
             }
         });
     }
